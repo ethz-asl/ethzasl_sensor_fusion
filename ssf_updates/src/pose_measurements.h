c5bfb592de872065a605adcfbf993db9489957c2
Markus Achtelik <markus.achtelik@mavt.ethz.ch>
2012-06-15 13:42:50 +0200

    initial values for p_ic and q_ci are now parameters

--- ssf_updates/src/pose_measurements.h
+++ ssf_updates/src/pose_measurements.h
@@ -18,14 +18,34 @@ public:
 	PoseMeasurements()
 	{
 		addHandler(new PoseSensorHandler(this));
+
+		ros::NodeHandle pnh("~");
+                pnh.param("init/p_ic/x", p_ic_[0], 0.0);
+                pnh.param("init/p_ic/y", p_ic_[1], 0.0);
+                pnh.param("init/p_ic/z", p_ic_[2], 0.0);
+
+                pnh.param("init/q_ci/w", q_ci_.w(), 1.0);
+                pnh.param("init/q_ci/x", q_ci_.x(), 0.0);
+                pnh.param("init/q_ci/y", q_ci_.y(), 0.0);
+                pnh.param("init/q_ci/z", q_ci_.z(), 0.0);
+
+                if(q_ci_.norm() != 1.0)
+                {
+                  ROS_WARN_STREAM("initial q_ci" << q_ci_.coeffs().transpose() <<") is not normalized, normalizing it now ... ");
+                  q_ci_.normalize();
+                }
 	};
 
 private:
+
+	Eigen::Matrix<double, 3, 1> p_ic_; ///< distance camera-IMU
+	Eigen::Quaternion<double> q_ci_;  ///< rotation camera-IMU
+
 	void init(double scale)
 	{
-		Eigen::Matrix<double, 3, 1> p,v,b_w,b_a,p_ic,g,w_m,a_m;
-		Eigen::Quaternion<double> q, q_ci,q_wv;
-		Eigen::Matrix<double,nState_,nState_> P;
+                Eigen::Matrix<double, 3, 1> p, v, b_w, b_a, g, w_m, a_m;
+                Eigen::Quaternion<double> q, q_wv;
+                SSF_Core::ErrorStateCov P;
 
 		// init values
 		g << 0, 0, 9.81;	/// gravity
@@ -36,13 +56,9 @@ private:
 		w_m << 0,0,0;		/// initial angular velocity
 		a_m =g;				/// initial acceleration
 
-		p_ic = Eigen::Matrix<double,3,1>(0, 0, 0);	/// distance camera-IMU
-//		q_ci=Eigen::Quaternion<double>(0, 1, 0, 0);	/// rotation camera-IMU
-q_ci=Eigen::Quaternion<double>(0, 1, -1, 0);
-		q_ci.normalize();
-		q_wv=Eigen::Quaternion<double>(1, 0, 0, 0);	/// vision-world rotation drift
-		q_wv.normalize();
-		P = Eigen::Matrix<double,nState_,nState_>::Constant(0);	/// error state covariance
+		q_wv.setIdentity();	/// vision-world rotation drift
+
+		P.setZero();	/// error state covariance
 
 		// check if we have already input from the measurement sensor
 		if(p_vc_.norm()==0)
@@ -51,12 +67,12 @@ q_ci=Eigen::Quaternion<double>(0, 1, -1, 0);
 			ROS_WARN_STREAM("No measurements received yet to initialize attitude - using [1 0 0 0]");
 
 		// calculate initial attitude and position based on sensor measurements
-		q = ( q_ci * q_cv_.conjugate() *q_wv).conjugate();
+		q = ( q_ci_ * q_cv_.conjugate() *q_wv).conjugate();
 		q.normalize();
-		p = q_wv.conjugate().toRotationMatrix()*p_vc_/scale - q.toRotationMatrix()*p_ic ;
+		p = q_wv.conjugate().toRotationMatrix()*p_vc_/scale - q.toRotationMatrix()*p_ic_ ;
 
 		// call initialization in core
-		ssf_core_.initialize(p,v,q,b_w,b_a,scale,q_wv,P,w_m,a_m,g,q_ci,p_ic);
+		ssf_core_.initialize(p,v,q,b_w,b_a,scale,q_wv,P,w_m,a_m,g,q_ci_,p_ic_);
 
 		ROS_INFO_STREAM("filter initialized to: \n" <<
 						"position: [" << p[0] << ", " << p[1] << ", " << p[2] << "]" <<
